33a410bd8b86a094b4585f6de42fc6bc3e46a500
joshua-smith98 <67178445+joshua-smith98@users.noreply.github.com>
2022-04-25 07:26:13 +1000

    Brought JoshsDSP.Console main into top level.

--- JoshsDSP.Console/Program.cs
+++ JoshsDSP.Console/Program.cs
@@ -1,48 +1,39 @@
 ﻿using SysConsole = System.Console;
 using JoshsDSP.Core;
 
-namespace JoshsDSP.Console
+bool IsRunning = true;
+string? cmd;
+
+while (IsRunning)
 {
-    class Program
+    SysConsole.Write(">");
+    cmd = SysConsole.ReadLine();
+
+    //Ignore command if null (should never be true)
+    if (cmd is null) continue;
+
+    //Split args by space
+    string[] cmdArgs = cmd.Split(' ');
+    if (cmdArgs.Length <= 0) continue;
+
+    string concat;
+
+    //TBI (eventually): an actual command system; this current one is really basic
+    switch (cmdArgs[0])
     {
-        static void Main(string[] args)
-        {
-            bool IsRunning = true;
-            string? cmd;
-
-            while (IsRunning)
-            {
-                SysConsole.Write(">");
-                cmd = SysConsole.ReadLine();
-
-                //Ignore command if null (should never be true)
-                if (cmd is null) continue;
-
-                //Split args by space
-                string[] cmdArgs = cmd.Split(' ');
-                if (cmdArgs.Length <= 0) continue;
-
-                string concat;
-
-                //TBI (eventually): an actual command system; this current one is really basic
-                switch (cmdArgs[0])
-                {
-                    case "play":
-                        concat = string.Join(' ', cmdArgs.Skip(1));
-                        if (!GlobalDSP.PlayFile(concat)) SysConsole.WriteLine("Failed to play file.");
-                        break;
-                    case "stop":
-                        if (cmdArgs.Length > 1) SysConsole.WriteLine("Expected zero arguments.");
-                        else if (!GlobalDSP.Stop()) SysConsole.WriteLine("Failed to stop playing.");
-                        break;
-                    case "exit":
-                        IsRunning = false;
-                        break;
-                    default:
-                        SysConsole.WriteLine("Command not recognised.");
-                        break;
-                }
-            }
-        }
+        case "play":
+            concat = string.Join(' ', cmdArgs.Skip(1));
+            if (!GlobalDSP.PlayFile(concat)) SysConsole.WriteLine("Failed to play file.");
+            break;
+        case "stop":
+            if (cmdArgs.Length > 1) SysConsole.WriteLine("Expected zero arguments.");
+            else if (!GlobalDSP.Stop()) SysConsole.WriteLine("Failed to stop playing.");
+            break;
+        case "exit":
+            IsRunning = false;
+            break;
+        default:
+            SysConsole.WriteLine("Command not recognised.");
+            break;
     }
 }
