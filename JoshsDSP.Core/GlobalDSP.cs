e36ef4af327655c204c50fe44d197137c7dc176a
joshua-smith98 <67178445+joshua-smith98@users.noreply.github.com>
2022-04-24 16:13:09 +1000

    Added basic console global play/stop

---
+++ JoshsDSP.Core/GlobalDSP.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+using NAudio.Wave;
+
+namespace JoshsDSP.Core
+{
+    public static class GlobalDSP
+    {
+        static DirectSoundOut _outputDevice;
+        static AudioFileReader _audioFileReader;
+
+        public static bool PlayFile(string dir)
+        {
+            if (!File.Exists(dir)) return false;
+
+            _audioFileReader?.Dispose();
+            _audioFileReader = new AudioFileReader(dir);
+            _outputDevice?.Dispose();
+            _outputDevice = new DirectSoundOut();
+            _outputDevice.Init(_audioFileReader);
+            _outputDevice.Play();
+
+            return true;
+        }
+
+        public static bool Stop()
+        {
+            if (_outputDevice is null || _outputDevice.PlaybackState == PlaybackState.Stopped) return false;
+            if (_audioFileReader is null) return false;
+
+            _outputDevice.Stop();
+
+            return true;
+        }
+    }
+}
