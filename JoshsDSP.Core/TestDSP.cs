7859212f5dcec9d9797ab9b2a72d362df26f6e1b
joshua-smith98 <67178445+joshua-smith98@users.noreply.github.com>
2022-04-25 16:37:32 +1000

    Implemented samplesCounter in TestDSP

--- JoshsDSP.Core/TestDSP.cs
+++ JoshsDSP.Core/TestDSP.cs
@@ -6,9 +6,7 @@ namespace JoshsDSP.Core
     {
         protected override void Process()
         {
-            double multiplier = (Math.Sin((double)(DateTime.Now.Ticks) * 0.0000001) / 2) + 0.5f;
-
-            //Console.WriteLine(multiplier);
+            double multiplier = (Math.Sin(_samplesCounter * 0.0005) / 2) + 0.5f;
 
             for (int i = 0; i < _currentSamples.Length; i++) _currentSamples[i] *= (float)multiplier;
         }
