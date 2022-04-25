ffc27d1afdab7dc45a643b352f585e65a7117674
joshua-smith98 <67178445+joshua-smith98@users.noreply.github.com>
2022-04-25 16:03:23 +1000

    Created quick TestDSP class to test the DSP works

---
+++ JoshsDSP.Core/TestDSP.cs
@@ -0,0 +1,14 @@
+﻿using NAudio.Wave;
+
+namespace JoshsDSP.Core
+{
+    public class TestDSP : BaseDSP
+    {
+        public TestDSP(ISampleProvider source) : base(source) { }
+        
+        protected override void Process()
+        {
+            for (int i = 0; i < _currentSamples.Length; i++) _currentSamples[i] *= ((float)(Math.Sin(DateTime.Now.Ticks * 0.1f)) / 2) + 0.5f;
+        }
+    }
+}
