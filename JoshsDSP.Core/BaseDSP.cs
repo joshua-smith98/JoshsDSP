8dbdff096ddb8ad302775dccd2f78cd1bb56b8cf
joshua-smith98 <67178445+joshua-smith98@users.noreply.github.com>
2022-04-25 15:55:39 +1000

    Created BaseDSP class

---
+++ JoshsDSP.Core/BaseDSP.cs
@@ -0,0 +1,43 @@
+﻿using NAudio.Wave;
+
+namespace JoshsDSP.Core
+{
+    public abstract class BaseDSP : ISampleProvider
+    {
+        protected ISampleProvider _sourceProvider;
+        
+        protected float[] _currentSamples;
+
+        public WaveFormat WaveFormat => _sourceProvider.WaveFormat;
+
+        public BaseDSP(ISampleProvider source)
+        {
+            _sourceProvider = source;
+            _currentSamples = new float[WaveFormat.Channels];
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int numSamples = _sourceProvider.Read(buffer, offset, count);
+
+            for (int i = offset; i < numSamples;  i += WaveFormat.Channels)
+            {
+                for (int channel = 0; channel < WaveFormat.Channels; channel++)
+                {
+                    _currentSamples[channel] = buffer[i + channel];
+                }
+
+                Process();
+
+                for (int channel = 0; channel < WaveFormat.Channels; channel++)
+                {
+                    buffer[i + channel] = _currentSamples[channel];
+                }
+            }
+
+            return numSamples;
+        }
+
+        protected abstract void Process();
+    }
+}
